1814a890c1af1b86f1fc831f6fad4a42172d936b
jamster10 <kpac.ja@gmail.com>
2019-05-09 17:42:47 -0500

    updated 404 middleware

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 
 
 app.use('*', (req, res, next) => {
-  res.status(404).send('Resource not Found');
+  res.status(404).json({message: 'Resource not Found'});
 });
 
 app.use(errorHandler);
